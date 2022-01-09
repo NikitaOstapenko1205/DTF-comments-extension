4174f288d525d6c477832ba8d1d19f104dbd54d6
nikitaPC <nikitaostapenko12@gmail.com>
2022-01-09 20:39:04 +0200

    Fix bug after changed comments block and add info about vote block

--- content.js
+++ content.js
@@ -1,32 +1,34 @@
 const linksToPagesWithComments = [
+	'content-link',
 	'content-feed__link',
 	'widget_comment__content__text',
 	'widget_comment__entry',
 	'live__item__text',
-	'u-notification__link',
-	'profile_comment_favorite__title'
+	'profile_comment_favorite__title',
+	'pddddbe2',
+	'ijj670d0'
 ]
 
-// const elemsDisabledForClicks = ['comments__item__collapse_subtree', 'comments__item__expand'];
+window.addEventListener('load', (event) => {
+	checkPageOnCommentsBlock();
 
-window.addEventListener('load', (event) => checkPageOnCommentsBlock());
+	window.addEventListener('click', (event) => {
+		event.stopPropagation();
 
-window.addEventListener('click', (event) => {
-	event.stopPropagation();
+		if (linksToPagesWithComments.some((elem) => {
+			return event.target.classList.contains(elem) || event.target.parentElement.classList.contains(elem);
+		})) {
+			setTimeout(() => {
+				checkPageOnCommentsBlock();
+			}, 3500);
+		}
+	});
 
-	if (linksToPagesWithComments.some((elem) => {
-		return event.target.classList.contains(elem) || event.target.parentElement.classList.contains(elem);
-	})) {
+	window.addEventListener('popstate', (event) => {
 		setTimeout(() => {
 			checkPageOnCommentsBlock();
 		}, 3500);
-	}
-});
-
-window.addEventListener('popstate', (event) => {
-	setTimeout(() => {
-		checkPageOnCommentsBlock();
-	}, 3500);
+	});
 });
 
 chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
@@ -41,7 +43,85 @@ const checkPageOnCommentsBlock = () => {
 		chrome.storage.sync.get(['hideComments', 'collapseInitialComments', 'collapseAllComments'], (commentsOption) => {
 			toggleCommentsBlock(commentsBlock, commentsOption);
 		});
+
+		if (!commentsBlock.classList.contains('make-votes')) {
+			const comments = Array.from(commentsBlock.querySelectorAll('.comment'));
+			if (comments.length > 0) {
+				makeRealVotesBlock(comments);
+				commentsBlock.classList.add('make-votes');
+			}
+
+			const observer = new MutationObserver(() => {
+				const comments = Array.from(commentsBlock.querySelectorAll('.comment:not(.comment--vote)'));
+
+				if (comments.length > 0) {
+					makeRealVotesBlock(comments);
+				}
+			});
+			observer.observe(commentsBlock.querySelector('.comments__content'), {childList: true});
+		}
 	}
+}
+
+const makeRealVotesBlock = (comments) => {
+	comments.map(async (comment) => {
+		await comment.querySelector('.vote__value').addEventListener('mouseenter', async (e) => {
+
+			if (!e.target.classList.contains('got-real-vote')) {
+				const likesDis = await getLikeDis(comment);
+				await makeHTMLVotes(e.target, likesDis.like, likesDis.dis);
+
+				const observer = new MutationObserver(async () => {
+					if (e.target.childElementCount === 0) {
+						const likesDis = await getLikeDis(comment);
+						makeHTMLVotes(e.target, likesDis.like, likesDis.dis);
+					}
+				});
+				observer.observe(e.target, {childList: true});
+
+				comment.classList.add('.comment--vote');
+			}
+
+		});
+	});
+}
+
+const getLikeDis = async (comment) => {
+	const projectHostName = window.location.host;
+
+	return await fetch(`https://${projectHostName}/vote/get_likers?id=${comment.dataset.id}&type=4&mode=raw`).
+		then((response) => {
+			if (response.ok) {
+				return response.json();
+			}
+		}).
+		then((response) => {
+			return Object.values(response.data.likers).
+				map((value) => {
+					return value.sign;
+				}).
+				reduce((acc, val) => {
+						const like = val === 1 ? (acc.like + 1) : acc.like;
+						const dis = val === -1 ? (acc.dis + 1) : acc.dis;
+						return { like, dis };
+					},
+					{like: 0, dis: 0}
+				);
+		}).catch((err) => console.log(err));
+}
+
+const makeHTMLVotes = (voteBlock, like, dis) => {
+	const likeSpan = document.createElement('span');
+	const disSpan = document.createElement('span');
+	likeSpan.style.cssText = 'color: #2ea83a;';
+	disSpan.style.cssText = 'color: #e52e3a';
+	likeSpan.innerHTML = `${like} /&nbsp;`;
+	disSpan.innerText = `${dis}`;
+
+	voteBlock.innerText = '';
+	voteBlock.append(likeSpan);
+	voteBlock.append(disSpan);
+	voteBlock.classList.add('got-real-vote');
 }
 
 const toggleBlockWithCat = (commentsBlock, isHideComments) => {
@@ -85,49 +165,31 @@ const toggleCommentsBlock = (commentsBlock, commentsOption) => {
 
 	if (!commentsOption.hideComments) {
 		const collapseElems = commentsOption.collapseAllComments ? 'all' : 'initial';
-		collapseComments(commentsBlock, collapseElems);
+		const commentsInitialArr = Array.from(commentsBlock.querySelectorAll("[data-reply_to='0']"));
+		collapseComments(commentsBlock, commentsInitialArr, collapseElems);
 	}
 }
 
-const collapseComments = (commentsBlock, collapseElems) => {
-	const commentsInitialArr = Array.from(commentsBlock.querySelector('.comments__content').children);
-	const commentsInternalArr = commentsInitialArr && commentsInitialArr.reduce(
-		(acc, initialComment) => {
-			return [...acc, ...Array.from(initialComment.querySelectorAll('.comment'))];
-		}, []);
+const collapseComments = (commentsBlock, comments, collapseOption) => {
+	const scrollPositionBeforeCollapse = window.scrollY;
 
-	collapseSingleComment([...commentsInitialArr, ...commentsInternalArr], 'close');
+	Array.from(commentsBlock.querySelectorAll('.comment__expand-branch--visible')).map((openComments) => {
+		openComments.click();
+	});
 
-	if (collapseElems === 'initial') {
-		collapseSingleComment(commentsInternalArr, 'open');
-	}
-}
-
-const collapseSingleComment = (comments, collapseOption) => {
 	comments.map((comment) => {
+		const id = comment.dataset.id;
+		const firstReplyComment = commentsBlock.querySelector(`[data-reply_to="${id}"]`);
 
-		const commentChildren = comment.querySelector('.comment__children');
-		let needClick = false;
-
-		if (
-			collapseOption === 'open' ||
-			(collapseOption === 'close' && !comment.classList.contains('comment--collapsed'))
-		) {
-			needClick = true;
-		}
-
-		if (commentChildren.childElementCount > 0 && needClick) {
-			let collapseChildrenElem;
-
-			if (collapseOption === 'close') {
-				collapseChildrenElem = commentChildren.previousElementSibling;
-			} else if (commentChildren.parentElement.querySelector('.comment__expand')) {
-				collapseChildrenElem = commentChildren.nextElementSibling;
-			}
-
-			const scrollPositionBeforeCollapse = window.scrollY;
-			!!collapseChildrenElem && collapseChildrenElem.click();
-			window.scrollTo(0, scrollPositionBeforeCollapse);
+		if (!!firstReplyComment) {
+			const firstReplyCommentBrunch = firstReplyComment.querySelector('.comment__branch');
+			!!firstReplyCommentBrunch && firstReplyCommentBrunch.click();
 		}
 	});
+
+	if ( collapseOption === 'all') {
+		Array.from(commentsBlock.querySelectorAll('.comment__branch--no-border')).map((brunch) => brunch.click());
+	}
+
+	window.scrollTo(0, scrollPositionBeforeCollapse);
 }
